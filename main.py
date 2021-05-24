dba1e2eb32edc51b2c572f0fd4457aa30db7855a
prakhar_36 <prakhargupta3680@gmail.com>
2021-05-24 09:02:59 +0530

    trying to optimize the size of png's

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def all_images():
         x = Image.open(f"./{jpg_img[i]}") 
         filename = jpg_img[i]
         img_original =  filename.rsplit( ".", 1 )[ 0 ]
-        x.save(f"{img_original}.png",dpi=[10,10],quality = 50)
+        x.save(f"{img_original}.png",quality = 10,optimize=True)
     
 def single_image():
     file_path = input("Enter jpg image name to convert (not extension) : ")
     x = Image.open(f"./{file_path}.jpg")
-    x.save(f"{file_path}.png",dpi=[300,300],quality = 50)
+    x.save(f"{file_path}.png",quality = 10)
 
 def user():
     ask_user = int(input("Do you want to convert all jpg images (1) or single jpg image (2) :"))
